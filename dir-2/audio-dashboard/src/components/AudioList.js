5104091d59225c8e1f3c080b1ae80283df3beb15
jessy521 <jessykumarsarkar@gmail.com>
2023-08-27 10:43:09 +0530

    comments added

--- dir-2/audio-dashboard/src/components/AudioList.js
+++ dir-2/audio-dashboard/src/components/AudioList.js
@@ -3,17 +3,21 @@ import Table from 'react-bootstrap/Table';
 import axios from "axios";
 
 const AudioList = () => {
+   // State to hold the audio file data
   const [files, setFiles] = useState([]);
 
-  
+  // Ref for accessing the audio element
   const audioRef = useRef();
   
+  // Callback for when metadata of the audio is loaded
   const onLoadedMetadata = () => {
     if (audioRef.current) {
       console.log((audioRef.current.duration/60).toFixed(2));
     }
   };
   
+
+ // Fetching data from the API and setting it in the 'files' state
   useEffect(() => {
     async function fetchdata() {
     const response = await axios.get("http://localhost:3001/api/audio");
@@ -22,24 +26,24 @@ const AudioList = () => {
     fetchdata();
   }, []);
   
+  // Rendering the component
   return (
-   
   <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
   <Table className="table">
     <thead className="bg-body-tertiary" style={{ backgroundColor: "#333", color: "white" }}>
-      <tr>
+          <tr>
+            {/* Heads of the table */}
         <th scope="col">Song No</th>
-            <th scope="col">File Name</th>
-            <th scope="col">Duration</th>
+        <th scope="col">File Name</th>
+        <th scope="col">Duration</th>
         <th scope="col">Size</th>
-        {/* <th scope="col">Duration</th> */}
         <th scope="col">Date of upload</th>
         <th scope="col">Play</th>
       </tr>
     </thead>
         <tbody>
+          {/* Mapping over each file in the 'files' state */}
           {files.map((file, index) => (
-            
         <tr key={index}>
           <th style={{ borderBottom: "none" }} scope="row">{index + 1}</th>
               <td style={{ borderBottom: "none" }}>{file.name}</td>
@@ -47,15 +51,18 @@ const AudioList = () => {
           <td style={{ borderBottom: "none" }}>{file.fileSize} bytes</td>
           <td style={{ borderBottom: "none" }}>{new Date(file.dateOfUpload).toLocaleDateString()}</td>
           <td style={{ borderBottom: "none" ,width:"10%"}}>
-            <div style={{ display: "flex", alignItems: "center" }}>
-              <audio controls ref={audioRef} onLoadedMetadata={onLoadedMetadata} style={{ width: "100%", maxWidth: "200px" }}>
+                <div style={{ display: "flex", alignItems: "center" }}>
+                  {/* Audio element */}
+                  <audio controls ref={audioRef} onLoadedMetadata={onLoadedMetadata} style={{ width: "100%", maxWidth: "200px" }}>
+                    {/* Source for audio playback */}
                     <source src={`http://localhost:3001/api/audio/${file.src}`} />
                 Your browser does not support the audio element.
-              </audio>
+                  </audio>
+                  {/* Progress bar */}
               <div style={{ flex: 1, marginLeft: "10px" }}>
                 <progress
-                  value="0" // Set initial progress value
-                  max="1"   // Set maximum value (you can set this to the duration of the audio)
+                  value="0" //  initial progress value
+                  max="1"   //  maximum value 
                   style={{ width: "100%", height: "8px" }} // Adjust width and height as needed
                 ></progress>
               </div>
--- dir-2/audio-dashboard/src/components/Header.js
+++ dir-2/audio-dashboard/src/components/Header.js
@@ -1,11 +1,14 @@
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 
+// Component rendering the heading
 function Heading() {
   return (
     <Container>
+      {/* Top navigation bar */}
       <Navbar expand="lg" className="bg-body-tertiary" style={{margin: "25px 50px 50px 50px",color: "LightGray"}}>
         <Container>
+          {/* Heading within the navigation bar */}
           <Navbar.Brand href="#"><b>Audio Dashboard</b></Navbar.Brand>
         </Container>
       </Navbar>
